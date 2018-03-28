a0d081536e25613d4f178e483fc723717d43be32
do <395614269@163.com>
2018-03-28 18:05:45 +0800

    完成6.824 lab1

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -2,8 +2,8 @@ package mapreduce
 
 import (
 	"fmt"
-	//"sync"
 	"sync"
+	"errors"
 )
 
 //
@@ -39,29 +39,53 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	for i := 0; i < ntasks; i++ {
 		wg.Add(1)
 		println("111111:",mapFiles[i])
-		go func() {
-			println("222222222:",mapFiles[i])
+
+		var doTask DoTaskArgs
+		doTask.File = mapFiles[i]
+		doTask.JobName = jobName
+		doTask.Phase = phase
+		doTask.TaskNumber = i
+		doTask.NumOtherPhase = n_other
+
+		//一定要传值！！！不然go并行运行，下面的值会引起混乱
+		go func(doTask DoTaskArgs) {
 			defer wg.Done()
 
-			println("filename:",mapFiles[i])
-			var doTask DoTaskArgs
-			doTask.File = mapFiles[i]
-			doTask.JobName = jobName
-			doTask.Phase = phase
-			doTask.TaskNumber = i
-			doTask.NumOtherPhase = n_other
+			for{
+				err := sendWork(doTask, registerChan)
+				fmt.Printf("doTask:%v.%v.%v",doTask.File, doTask.Phase, doTask.TaskNumber)
+				if err == nil{
+					break
+				}
+			}
 
-			ch_worker := <- registerChan
-			call(ch_worker, "Worker.DoTask", doTask, nil)
-			//ok := call(ch_worker, "Worker.DoTask", doTask, nil)
-			//if ok{
-			//		registerChan <- ch_worker
-			//	}
-
-			}()
-			debug("schedule...i:%s, mapFileName:%s", i, mapFiles[i])
+		}(doTask)
+		fmt.Printf("schedule...i:%v, mapFileName:%v\n", i, mapFiles[i])
 	}
 
 	wg.Wait()
+
 	fmt.Printf("Schedule: %v done\n", phase)
+}
+
+func sendWork(doTask DoTaskArgs, registerChan chan string) error {
+	println("111111:",doTask.File, doTask.Phase,doTask.TaskNumber)
+
+	ch_worker := <- registerChan
+	println("222222:",doTask.File, doTask.Phase,doTask.TaskNumber)
+	ok := call(ch_worker, "Worker.DoTask", doTask, nil)
+	//worker工作完成，再次放在注册列表等待分配任务
+	println("3333333:",doTask.File, doTask.Phase,doTask.TaskNumber, ok)
+	if ok{
+	//用go执行，不会阻塞进程。
+	//最后几个registerChan不会有人读，所以这里不会go会卡死
+	//主进程结束，这里也会停止
+		go func() {
+			registerChan <- ch_worker
+		}()
+		return nil
+	}
+	println("444444:",doTask.File, doTask.Phase,doTask.TaskNumber, ok)
+	var err error = errors.New("worker 调用失败")
+	return err
 }
